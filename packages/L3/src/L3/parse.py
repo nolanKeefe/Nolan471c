098843ac008b01b0df82753314d165e1bfaaa037
nolanKeefe <npkeefe@udel.edu>
2026-03-06 13:22:39 -0500

    parse.py should be done now but need to check the tests

--- packages/L3/src/L3/parse.py
+++ packages/L3/src/L3/parse.py
@@ -4,7 +4,23 @@ from pathlib import Path
 from lark import Lark, Token, Transformer
 from lark.visitors import v_args  # pyright: ignore[reportUnknownVariableType]
 
-from .syntax import Abstract, Apply, Identifier, Immediate, Let, LetRec, Primitive, Program, Reference, Term
+from .syntax import (
+    Abstract,
+    Allocate,
+    Apply,
+    Begin,
+    Branch,
+    Identifier,
+    Immediate,
+    Let,
+    LetRec,
+    Load,
+    Primitive,
+    Program,
+    Reference,
+    Store,
+    Term,
+)
 
 
 class AstTransformer(Transformer[Token, Program | Term]):
@@ -119,6 +135,45 @@ class AstTransformer(Transformer[Token, Program | Term]):
             left=left,
             right=right,
         )
+
+    @v_args(inline=True)
+    def branch(
+        self,
+        operator: Token,
+        left: Term,
+        right: Term,
+        consequent: Term,
+        otherwise: Term,
+    ) -> Term:
+        return Branch(
+            # same thing as prev where we ignore the string things
+            operator=str(operator),  # pyright: ignore[reportArgumentType]
+            left=left,
+            right=right,
+            consequent=consequent,
+            otherwise=otherwise,
+        )
+
+    @v_args(inline=True)
+    def allocate(self, count: Token) -> Term:
+        return Allocate(
+            count=int(count),
+        )
+
+    @v_args(inline=True)
+    def load(self, base: Term, index: Token) -> Term:
+        return Load(base=base, index=int(index))
+
+    @v_args(inline=True)
+    def store(self, base: Term, index: Token, value: Term) -> Term:
+        return Store(base=base, index=int(index), value=value)
+
+    # length varies can't inline it
+    def begin(
+        self,
+        effects: Sequence[Term],
+    ) -> Term:
+        return Begin(effects=list(effects[:-1]), value=effects[-1])
 
 
 def parse_term(source: str) -> Term:
